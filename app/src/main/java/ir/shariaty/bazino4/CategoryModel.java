27a7bf4d4657ce22926e9f5c42b0e4878b6c908e
Hossein <djalicrt@gmail.com>
2021-04-30 20:49:43 +0430

    impl MVVM Architecture Category Model - Bind - Adapter DashBoard Activity UI

---
+++ app/src/main/java/ir/shariaty/bazino4/CategoryModel.java
@@ -0,0 +1,39 @@
+package ir.shariaty.bazino4;
+
+public class CategoryModel {
+    private String categoryId, categoryName, categoryImage;
+
+    public CategoryModel(String categoryId, String categoryName, String categoryImage) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.categoryImage = categoryImage;
+    }
+
+    public CategoryModel () {
+        // ToDo
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryImage() {
+        return categoryImage;
+    }
+
+    public void setCategoryImage(String categoryImage) {
+        this.categoryImage = categoryImage;
+    }
+}
