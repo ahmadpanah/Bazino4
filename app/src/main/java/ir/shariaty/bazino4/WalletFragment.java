ab93284a8d3ca88ec690d17a9a1e14a2d33d2348
Hossein <djalicrt@gmail.com>
2021-05-05 18:24:54 +0430

    Wallet Fragment UI Done!

---
+++ app/src/main/java/ir/shariaty/bazino4/WalletFragment.java
@@ -0,0 +1,29 @@
+package ir.shariaty.bazino4;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WalletFragment extends Fragment {
+
+    public WalletFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_wallet, container, false);
+    }
+}
