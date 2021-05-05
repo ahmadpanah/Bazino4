a89838bff1eef53df7243e7a87becc05c5580654
Hossein <djalicrt@gmail.com>
2021-05-05 18:46:15 +0430

    LeaderBoard Fragment UI Done!

---
+++ app/src/main/java/ir/shariaty/bazino4/LeaderboardsFragment.java
@@ -0,0 +1,34 @@
+package ir.shariaty.bazino4;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LeaderboardsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LeaderboardsFragment extends Fragment {
+
+    public LeaderboardsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_leaderboards, container, false);
+    }
+}
