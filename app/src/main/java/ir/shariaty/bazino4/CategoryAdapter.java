e7bc77e91a054f2c6e6da2a00e9aa32baa2fb982
Hossein <djalicrt@gmail.com>
2021-05-05 16:42:29 +0430

    UI Impl. Home Activity Done! Glide Library added to project.

--- app/src/main/java/ir/shariaty/bazino4/CategoryAdapter.java
+++ app/src/main/java/ir/shariaty/bazino4/CategoryAdapter.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.ArrayList;
 
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder> {
@@ -38,7 +40,9 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
          CategoryModel model = categoryModels.get(position);
 
          holder.textView.setText(model.getCategoryName());
-         //TODO
+         Glide.with(context)
+              .load(model.getCategoryImage())
+              .into(holder.imageView);
    }
 
    @Override
--- app/src/main/java/ir/shariaty/bazino4/MainActivity.java
+++ app/src/main/java/ir/shariaty/bazino4/MainActivity.java
@@ -1,14 +1,55 @@
 package ir.shariaty.bazino4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import ir.shariaty.bazino4.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+       ArrayList<CategoryModel> categories = new ArrayList<>();
+       categories.add(new CategoryModel("","Sport","https://cdn4.iconfinder.com/data/icons/supermarket-34/512/sporting-goods-ball-sport-512.png"));
+       categories.add(new CategoryModel("","History","https://www.pinclipart.com/picdir/big/138-1388962_game-history-history-icon-vector-clipart.png"));
+       categories.add(new CategoryModel("","Puzzle","https://cdn4.iconfinder.com/data/icons/supermarket-34/512/sporting-goods-ball-sport-512.png"));
+       categories.add(new CategoryModel("","Cinema","https://cdn4.iconfinder.com/data/icons/supermarket-34/512/sporting-goods-ball-sport-512.png"));
+       categories.add(new CategoryModel("","Mathematics","https://cdn4.iconfinder.com/data/icons/supermarket-34/512/sporting-goods-ball-sport-512.png"));
+       categories.add(new CategoryModel("","Computer","https://cdn4.iconfinder.com/data/icons/supermarket-34/512/sporting-goods-ball-sport-512.png"));
+       categories.add(new CategoryModel("","Religion","https://cdn4.iconfinder.com/data/icons/supermarket-34/512/sporting-goods-ball-sport-512.png"));
+       CategoryAdapter adapter = new CategoryAdapter(this,categories);
+       binding.categoryList.setLayoutManager(new GridLayoutManager(this,2));
+       binding.categoryList.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.wallet) {
+            Toast.makeText(this,"Wallet is Clicked!",Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
