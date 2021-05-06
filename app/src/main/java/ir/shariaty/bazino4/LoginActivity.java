5418b7a005213748ccb5f1b9657980815318a854
Hossein <djalicrt@gmail.com>
2021-05-06 16:34:27 +0430

    Get Category List Item (ID + Name + Image) from Firestore Database

--- app/src/main/java/ir/shariaty/bazino4/LoginActivity.java
+++ app/src/main/java/ir/shariaty/bazino4/LoginActivity.java
@@ -34,6 +34,11 @@ public class LoginActivity extends AppCompatActivity {
         dialog = new ProgressDialog(this);
         dialog.setMessage("Logging in...");
 
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this , MainActivity.class));
+            finish();
+        }
+
         binding.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/ir/shariaty/bazino4/MainActivity.java
+++ app/src/main/java/ir/shariaty/bazino4/MainActivity.java
@@ -1,6 +1,7 @@
 package ir.shariaty.bazino4;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 
@@ -10,6 +11,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
 
 import ir.shariaty.bazino4.databinding.ActivityMainBinding;
@@ -17,6 +24,7 @@ import ir.shariaty.bazino4.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    FirebaseFirestore database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +34,26 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-       ArrayList<CategoryModel> categories = new ArrayList<>();
-       categories.add(new CategoryModel("","Sport","https://cdn4.iconfinder.com/data/icons/supermarket-34/512/sporting-goods-ball-sport-512.png"));
-       categories.add(new CategoryModel("","History","https://www.pinclipart.com/picdir/big/138-1388962_game-history-history-icon-vector-clipart.png"));
-       categories.add(new CategoryModel("","Puzzle","https://cdn4.iconfinder.com/data/icons/supermarket-34/512/sporting-goods-ball-sport-512.png"));
-       categories.add(new CategoryModel("","Cinema","https://cdn4.iconfinder.com/data/icons/supermarket-34/512/sporting-goods-ball-sport-512.png"));
-       categories.add(new CategoryModel("","Mathematics","https://cdn4.iconfinder.com/data/icons/supermarket-34/512/sporting-goods-ball-sport-512.png"));
-       categories.add(new CategoryModel("","Computer","https://cdn4.iconfinder.com/data/icons/supermarket-34/512/sporting-goods-ball-sport-512.png"));
-       categories.add(new CategoryModel("","Religion","https://cdn4.iconfinder.com/data/icons/supermarket-34/512/sporting-goods-ball-sport-512.png"));
+        database = FirebaseFirestore.getInstance();
+
+        ArrayList<CategoryModel> categories = new ArrayList<>();
+
        CategoryAdapter adapter = new CategoryAdapter(this,categories);
+
+       database.collection("categories")
+               .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                   @Override
+                   public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                      categories.clear();
+                       for (DocumentSnapshot snapshot : value.getDocuments()) {
+                           CategoryModel model = snapshot.toObject(CategoryModel.class);
+                           model.setCategoryId(snapshot.getId());
+                           categories.add(model);
+                       }
+                       adapter.notifyDataSetChanged();
+                   }
+               });
+
        binding.categoryList.setLayoutManager(new GridLayoutManager(this,2));
        binding.categoryList.setAdapter(adapter);
     }
