bfcba8334ac6de7dadbe95c5fad2f6907709e863
Hossein <djalicrt@gmail.com>
2021-05-10 16:18:53 +0430

    impl. Switch Case Fragments in Main Activity

--- app/src/main/java/ir/shariaty/bazino4/HomeFragment.java
+++ app/src/main/java/ir/shariaty/bazino4/HomeFragment.java
@@ -44,6 +44,8 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater,container,false);
 
+        database = FirebaseFirestore.getInstance();
+
         ArrayList<CategoryModel> categories = new ArrayList<>();
 
         CategoryAdapter adapter = new CategoryAdapter(getContext(),categories);
