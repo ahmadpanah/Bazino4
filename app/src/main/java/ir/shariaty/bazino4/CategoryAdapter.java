de2a9a3aca03292465b0730e6b2d8030ea835b1a
Hossein <djalicrt@gmail.com>
2021-05-10 18:50:10 +0430

    Fetch Questions From Database

--- app/src/main/java/ir/shariaty/bazino4/CategoryAdapter.java
+++ app/src/main/java/ir/shariaty/bazino4/CategoryAdapter.java
@@ -1,6 +1,7 @@
 package ir.shariaty.bazino4;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -43,6 +44,15 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
          Glide.with(context)
               .load(model.getCategoryImage())
               .into(holder.imageView);
+
+         holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent = new Intent(context , QuizActivity.class);
+               intent.putExtra("catId", model.getCategoryId());
+               context.startActivity(intent);
+            }
+         });
    }
 
    @Override
--- app/src/main/java/ir/shariaty/bazino4/QuizActivity.java
+++ app/src/main/java/ir/shariaty/bazino4/QuizActivity.java
@@ -2,13 +2,168 @@ package ir.shariaty.bazino4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Random;
+
+import ir.shariaty.bazino4.databinding.ActivityQuizBinding;
 
 public class QuizActivity extends AppCompatActivity {
+
+    ActivityQuizBinding binding;
+
+    ArrayList<Question> questions;
+    int index = 0;
+    Question question;
+    CountDownTimer timer;
+    FirebaseFirestore database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz);
+        binding = ActivityQuizBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        questions = new ArrayList<>();
+        database = FirebaseFirestore.getInstance();
+
+        final String catId = getIntent().getStringExtra("catId");
+
+        Random random = new Random();
+        int rand = random.nextInt(12);
+
+        //Todo BugFix!
+        database.collection("categories")
+                .document(catId)
+                .collection("questions")
+                .whereLessThanOrEqualTo("index", rand)
+                .orderBy("index")
+                .limit(5).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                                         @Override
+                                                         public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                                             if (queryDocumentSnapshots.getDocuments().size() < 5) {
+                                                                 database.collection("categories")
+                                                                         .document(catId)
+                                                                         .collection("questions")
+                                                                         .whereLessThanOrEqualTo("index", rand)
+                                                                         .orderBy("index")
+                                                                         .limit(5).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                                                     @Override
+                                                                     public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                                                             for (DocumentSnapshot snapshot : queryDocumentSnapshots) {
+                                                                                 Question question = snapshot.toObject(Question.class);
+                                                                                 questions.add(question);
+                                                                         }
+                                                                     }
+                                                                 });
+                                                             } else {
+                                                                 for (DocumentSnapshot snapshot : queryDocumentSnapshots) {
+                                                                     Question question = snapshot.toObject(Question.class);
+                                                                     questions.add(question);
+                                                                 }
+                                                             }
+                                                         }
+                                                     });
+
+
+        resetTimer();
+        setNextQuestion();
+    }
+
+    void resetTimer() {
+        timer = new CountDownTimer(20000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                binding.timer.setText(String.valueOf(millisUntilFinished/1000));
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
+    }
+
+    void showAnswer() {
+        if (question.getAnswer().equals(binding.option1.getText().toString()))
+            binding.option1.setBackground(getResources().getDrawable(R.drawable.option_right));
+        else if  (question.getAnswer().equals(binding.option2.getText().toString()))
+        binding.option2.setBackground(getResources().getDrawable(R.drawable.option_right));
+        else if  (question.getAnswer().equals(binding.option3.getText().toString()))
+            binding.option3.setBackground(getResources().getDrawable(R.drawable.option_right));
+        else if  (question.getAnswer().equals(binding.option4.getText().toString()))
+            binding.option4.setBackground(getResources().getDrawable(R.drawable.option_right));
+    }
+
+    void setNextQuestion() {
+        resetTimer();
+        if(timer != null)
+            timer.cancel();
+
+        timer.start();
+        if (index < questions.size()) {
+            binding.questionCounter.setText(String.format("%d/%d" , (index+1), questions.size()));
+            question = questions.get(index);
+            binding.question.setText(question.getQuestion());
+            binding.option1.setText(question.getOption1());
+            binding.option2.setText(question.getOption2());
+            binding.option3.setText(question.getOption3());
+            binding.option4.setText(question.getOption4());
+        }
+    }
+
+    void checkAnswer(TextView textView) {
+        String selectedAnswer = textView.getText().toString();
+        if (selectedAnswer.equals(question.getAnswer())) {
+            textView.setBackground(getResources().getDrawable(R.drawable.option_right));
+        } else {
+            showAnswer();
+            textView.setBackground(getResources().getDrawable(R.drawable.option_wrong));
+        }
+    }
+
+    void reset(){
+        binding.option1.setBackground(getResources().getDrawable(R.drawable.option_unselected));
+        binding.option2.setBackground(getResources().getDrawable(R.drawable.option_unselected));
+        binding.option3.setBackground(getResources().getDrawable(R.drawable.option_unselected));
+        binding.option4.setBackground(getResources().getDrawable(R.drawable.option_unselected));
+
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.option_1:
+            case R.id.option_2:
+            case R.id.option_3:
+            case R.id.option_4:
+                if (timer != null)
+                    timer.cancel();
+                TextView selected = (TextView) view;
+                checkAnswer(selected);
+                break;
+
+            case R.id.nextBtn:
+                reset();
+                if (index < questions.size()) {
+                    index++;
+                    setNextQuestion();
+                } else {
+                    Toast.makeText(this, "Quiz Finished!", Toast.LENGTH_SHORT).show();
+                }
+        }
     }
 }
