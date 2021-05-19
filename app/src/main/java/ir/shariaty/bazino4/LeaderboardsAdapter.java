2df920f180900414245251369570c024c9b4dba4
Hossein <djalicrt@gmail.com>
2021-05-19 17:23:45 +0430

    Finish Online Leaderboards Feature.

---
+++ app/src/main/java/ir/shariaty/bazino4/LeaderboardsAdapter.java
@@ -0,0 +1,53 @@
+package ir.shariaty.bazino4;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import ir.shariaty.bazino4.databinding.RowLeaderboardsBinding;
+
+public class LeaderboardsAdapter extends  RecyclerView.Adapter<LeaderboardsAdapter.LeaderboardViewHolder> {
+
+    Context context;
+    ArrayList<User> users;
+    public LeaderboardsAdapter(Context context, ArrayList<User> users) {
+        this.context = context;
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public LeaderboardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.row_leaderboards, parent, false);
+        return new LeaderboardViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull  LeaderboardsAdapter.LeaderboardViewHolder holder, int position) {
+        User user = users.get(position);
+
+        holder.binding.name.setText(user.getName());
+        holder.binding.coins.setText(String.valueOf(user.getCoins()));
+        holder.binding.index.setText(String.format("#%d" , position+1));
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public class LeaderboardViewHolder extends RecyclerView.ViewHolder {
+
+        RowLeaderboardsBinding binding;
+        public LeaderboardViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = RowLeaderboardsBinding.bind(itemView);
+        }
+    }
+}
